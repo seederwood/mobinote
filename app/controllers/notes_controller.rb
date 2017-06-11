ac9b0e7615d100c1f549f8d279472a1f290d75a3
Ray Climie <seederwood@gmail.com>
2017-06-11 18:19:58 -0300

    change index to show only current users notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,7 @@
 class NotesController < ApplicationController
   before_action :find_note, only: %i[show edit update destroy]
   def index
-    @notes = Note.all.order('created_at desc')
+    @notes = Note.where(user_id: current_user).order('created_at desc')
   end
 
   def show; end
