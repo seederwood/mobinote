70cdec207d77ee6416f0c348537dea620e33737f
Ray Climie <seederwood@gmail.com>
2017-06-11 17:44:17 -0300

    add devise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611023203) do
+ActiveRecord::Schema.define(version: 20170611204326) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
